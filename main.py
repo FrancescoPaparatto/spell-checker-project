80c74a4232993a5fd65448129526572f14457fd7
Francesco Paparatto <francescopaparatto@gmail.com>
2025-11-14 19:28:13 +0100

    added personal notes' files to the '.gitignore' file

--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from utils import load_file, create_dictionary
 
 
 def main():
+    # TODO: evaluate to create an helper function also for getting config files in order to handle JsonDecodeError 
     with open("config.json", "r") as file:
         config = json.load(file)
 
-    english_dictionary = create_dictionary(
-        load_file(config["dictionary"]["english_path"])
-    )
+    try:
+        english_dictionary = create_dictionary(
+            load_file(config["dictionary"]["english_path"])
+        )
 
-    business_dictionary = create_dictionary(
-        load_file(config["dictionary"]["business_path"])
-    )
+        business_dictionary = create_dictionary(
+            load_file(config["dictionary"]["business_path"])
+        )
+    except FileNotFoundError as e:
+        print(e)
 
-    dictionary = english_dictionary | business_dictionary
-
-
-
-
+    else:
+        dictionary = english_dictionary | business_dictionary
 
 
 if __name__ == "__main__":
